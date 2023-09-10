fac7de566cc3bb53601eced3b7187db3517dc1ba
Marcus_Ramalho <ramalhomarcusantonio@id.uff.br>
2023-09-09 23:12:15 -0300

    acerto mizeravi

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,35 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main(){
     println!("Adivinhe o número!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("O número secreto é: {secret_number}");
+    //println!("O número secreto é: {secret_number}");
+    loop {
+        println!("Digite o número que você chutou.");
 
-    println!("Digite o número que você chutou.");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+            io::stdin()//importa a função stdin da biblioteca io que está dentro da biblioteca std
+            .read_line(&mut guess)
+            .expect("Falha ao ler a linha");
+        
+        let guess:i32 = guess.trim().parse::<i32>().expect("Por favor digite um número!");
 
-        io::stdin()//importa a função stdin da biblioteca io que está dentro da biblioteca std
-        .read_line(&mut guess)
-        .expect("Falha ao ler a linha");
-    
-    println!("Você chutou: {guess}");
+        println!("Você chutou: {guess}");
 
-
-
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Ta frio!"),
+            Ordering::Greater => println!("Ta quente demais!"),
+            Ordering::Equal => {
+                println!("Acerto mizeravi!");
+                break;
+            }
+        }
+    }       
 }
 
 
